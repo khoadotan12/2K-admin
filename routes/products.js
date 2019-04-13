c540e75ffec6a3980d10842d6d96ab542d354145
Dong Kha <dongkha97@gmail.com>
2019-04-13 20:21:32 +0700

    add list product page

---
+++ routes/products.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res, next) => {
+    const price = [20990000, 28600000, 12990000];
+    const data = [{
+        name: 'Samsung Galaxy S10',
+        brand: 'Samsung',
+        count: 100,
+        price: price[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."),
+    }, {
+        name: 'iPhone XS Max 64 GB',
+        brand: 'Apple',
+        count: 542,
+        price: price[1].toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."),
+    }, {
+        name: 'Xiaomi Mi Mix 3',
+        brand: 'Xiaomi',
+        count: 0,
+        price: price[2].toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."),
+    },]
+    res.render('products/index', { category: 'Sản phẩm', categoryLink: '/product', title: 'Danh sách sản phẩm', data })
+});
+
+module.exports = router;

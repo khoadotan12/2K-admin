1b5be02e5b9c611a07b5229074113c87e3f46acc
Dong Kha <dongkha97@gmail.com>
2019-04-13 19:38:55 +0700

    change data table user layout

--- public/assets/js/init-scripts/data-table/datatables-init.js
+++ public/assets/js/init-scripts/data-table/datatables-init.js
@@ -6,11 +6,11 @@
     -------------*/
 
     $('#bootstrap-data-table').DataTable({
-        lengthMenu: [[10, 20, 50, -1], [10, 20, 50, "All"]],
+        lengthMenu: [[10, 20, 50, -1], [10, 20, 50, "Tất cả"]],
     });
 
     $('#bootstrap-data-table-export').DataTable({
-        lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "Tất cả"]],
         buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
     });
 
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,91 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.render('users/index', {root: 'Trang chủ', category: 'Tài khoản', title: 'Danh sách tài khoản'});
+  const users = [{
+    username: 'lukka',
+    email: 'lukka@optonline.net',
+    phone: '0496406146',
+  },
+  {
+    username: 'ryanvm',
+    email: 'ryanvm@aol.com',
+    phone: '146604560',
+  },
+  {
+    username: 'rmcfa',
+    email: 'rmcfarla@live.com',
+    phone: '146604560',
+  },
+  {
+    username: 'ranasta',
+    email: 'ranasta@me.com',
+    phone: '526980288',
+  },
+  {
+    username: 'sonnen',
+    email: 'sonnen@verizon.net',
+    phone: '7039393699',
+  },
+  {
+    username: 'containfizzle',
+    email: 'zwood@sbcglobal.net',
+    phone: '97000356256',
+  },
+  {
+    username: 'wilfernerd',
+    email: 'ghost@mac.com',
+    phone: '013431434',
+  },
+  {
+    username: 'sonnen',
+    email: 'sonnen@verizon.net',
+    phone: '7039393699',
+  },
+  {
+    username: 'containfizzle',
+    email: 'zwood@sbcglobal.net',
+    phone: '97000356256',
+  },
+  {
+    username: 'wilfernerd',
+    email: 'ghost@mac.com',
+    phone: '013431434',
+  },
+  {
+    username: 'wilfernerd',
+    email: 'ghost@mac.com',
+    phone: '013431434',
+  },
+  {
+    username: 'wilfernerd',
+    email: 'ghost@mac.com',
+    phone: '013431434',
+  },
+  {
+    username: 'sonnen',
+    email: 'sonnen@verizon.net',
+    phone: '7039393699',
+  },
+  {
+    username: 'containfizzle',
+    email: 'zwood@sbcglobal.net',
+    phone: '97000356256',
+  },
+  {
+    username: 'wilfernerd',
+    email: 'ghost@mac.com',
+    phone: '013431434',
+  },
+  {
+    username: 'wilfernerd',
+    email: 'ghost@mac.com',
+    phone: '013431434',
+  }]
+  res.render('users/index', { category: 'Tài khoản', categoryLink: '/users', title: 'Danh sách tài khoản', users: users });
 });
 
 router.get('/add', function (req, res, next) {
-  res.render('users/add', { root: 'Trang chủ', category: 'Tài khoản', title: 'Thêm tài khoản' });
+  res.render('users/add', { category: 'Tài khoản', categoryLink: '/users', title: 'Thêm tài khoản' });
 });
 
 module.exports = router;

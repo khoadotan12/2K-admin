413bd36ac281e81ae15696db16f4a57273e5a24f
Dong Kha <dongkha97@gmail.com>
2019-04-13 16:57:55 +0700

    add router for user

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/add', function (req, res, next) {
+  res.render('users/add', { title: 'Thêm tài khoản'});
 });
 
 module.exports = router;

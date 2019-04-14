7b6e011618397072cd6ee6a8b98b83a5c01c443b
Dong Kha <dongkha97@gmail.com>
2019-04-14 17:54:29 +0700

    change title revenue

--- routes/revenue.js
+++ routes/revenue.js
@@ -2,23 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/date', (req, res, next) => {
-    res.render('revenue/date', { category: 'Doanh thu', categoryLink: '/', title: 'Ngày' });
+    res.render('revenue/date', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong ngày' });
 });
 
 router.get('/week', (req, res, next) => {
-    res.render('revenue/week', { category: 'Doanh thu', categoryLink: '/', title: 'Tuần' });
+    res.render('revenue/week', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong tuần' });
 });
 
 router.get('/month', (req, res, next) => {
-    res.render('revenue/month', { category: 'Doanh thu', categoryLink: '/', title: 'Tháng' });
+    res.render('revenue/month', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong tháng' });
 });
 
 router.get('/quarter', (req, res, next) => {
-    res.render('revenue/quarter', { category: 'Doanh thu', categoryLink: '/', title: 'Quý' });
+    res.render('revenue/quarter', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong quý' });
 });
 
 router.get('/year', (req, res, next) => {
-    res.render('revenue/year', { category: 'Doanh thu', categoryLink: '/', title: 'Năm' });
+    res.render('revenue/year', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong năm' });
 });
 
 

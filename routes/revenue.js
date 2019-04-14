0d0b60cbd40e7f988c66fb2c0f621fbf44677ea7
Dong Kha <dongkha97@gmail.com>
2019-04-14 22:08:47 +0700

    create revenue week and put data

--- routes/revenue.js
+++ routes/revenue.js
@@ -47,7 +47,42 @@ router.get('/date', (req, res, next) => {
 });
 
 router.get('/week', (req, res, next) => {
-    res.render('revenue/week', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong tuần' });
+    const price = [6910000, 13940000, 31059000, 1099000, 9401000];
+    const data = [
+        {
+            name: 'Samsung Galaxy A7',
+            price: formatPrice(price[0]),
+            count: 18,
+        },
+        {
+            name: 'Xiaomi Mi Pro',
+            price: formatPrice(price[1]),
+            count: 9,
+        },
+        {
+            name: 'iPhone XS Max 256 GB',
+            price: formatPrice(price[2]),
+            count: 22,
+        },
+        {
+            name: 'Oppo Find X',
+            price: formatPrice(price[3]),
+            count: 10,
+        },
+        {
+            name: 'Samsung Galaxy A9',
+            price: formatPrice(price[4]),
+            count: 6,
+        },
+    ];
+    let sum = 0;
+    data.forEach((element, index) => {
+        element.id = (index + 1).toString();
+        element.total = formatPrice(price[index] * element.count);
+        sum += (price[index] * element.count);
+    });
+    const totalSum = formatPrice(sum);
+    res.render('revenue/week', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong tuần', data, totalSum });
 });
 
 router.get('/month', (req, res, next) => {

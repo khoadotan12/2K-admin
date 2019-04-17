2e64185764c3ee05a8e2b7813116327061cc8ca1
Dong Kha <dongkha97@gmail.com>
2019-04-17 17:07:56 +0700

    add month, quarter and year page

--- routes/revenue.js
+++ routes/revenue.js
@@ -6,95 +6,61 @@ function formatPrice(price) {
 }
 
 const sumArr = arr => arr.reduce((a, b) => a + b, 0);
+const price = [6910000, 13940000, 31059000, 1099000, 9401000];
+const data = [
+    {
+        name: 'Samsung Galaxy A7',
+        price: formatPrice(price[0]),
+        count: 18,
+    },
+    {
+        name: 'Xiaomi Mi Pro',
+        price: formatPrice(price[1]),
+        count: 9,
+    },
+    {
+        name: 'iPhone XS Max 256 GB',
+        price: formatPrice(price[2]),
+        count: 22,
+    },
+    {
+        name: 'Oppo Find X',
+        price: formatPrice(price[3]),
+        count: 10,
+    },
+    {
+        name: 'Samsung Galaxy A9',
+        price: formatPrice(price[4]),
+        count: 6,
+    },
+];
+let sum = 0;
+data.forEach((element, index) => {
+    element.id = (index + 1).toString();
+    element.total = formatPrice(price[index] * element.count);
+    sum += (price[index] * element.count);
+});
+const totalSum = formatPrice(sum);
+const years = [2016, 2017, 2018];
 
 router.get('/date', (req, res, next) => {
-    const price = [6910000, 13940000, 31059000, 1099000, 9401000];
-    const data = [
-        {
-            name: 'Samsung Galaxy A7',
-            price: formatPrice(price[0]),
-            count: 1,
-        },
-        {
-            name: 'Xiaomi Mi Pro',
-            price: formatPrice(price[1]),
-            count: 4,
-        },
-        {
-            name: 'iPhone XS Max 256 GB',
-            price: formatPrice(price[2]),
-            count: 2,
-        },
-        {
-            name: 'Oppo Find X',
-            price: formatPrice(price[3]),
-            count: 1,
-        },
-        {
-            name: 'Samsung Galaxy A9',
-            price: formatPrice(price[4]),
-            count: 3,
-        },
-    ];
-    let sum = 0;
-    data.forEach((element, index) => {
-        element.id = (index + 1).toString();
-        element.total = formatPrice(price[index] * element.count);
-        sum += (price[index] * element.count);
-    });
-    const totalSum = formatPrice(sum);
     res.render('revenue/date', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong ngày', data, totalSum });
 });
 
 router.get('/week', (req, res, next) => {
-    const price = [6910000, 13940000, 31059000, 1099000, 9401000];
-    const data = [
-        {
-            name: 'Samsung Galaxy A7',
-            price: formatPrice(price[0]),
-            count: 18,
-        },
-        {
-            name: 'Xiaomi Mi Pro',
-            price: formatPrice(price[1]),
-            count: 9,
-        },
-        {
-            name: 'iPhone XS Max 256 GB',
-            price: formatPrice(price[2]),
-            count: 22,
-        },
-        {
-            name: 'Oppo Find X',
-            price: formatPrice(price[3]),
-            count: 10,
-        },
-        {
-            name: 'Samsung Galaxy A9',
-            price: formatPrice(price[4]),
-            count: 6,
-        },
-    ];
-    let sum = 0;
-    data.forEach((element, index) => {
-        element.id = (index + 1).toString();
-        element.total = formatPrice(price[index] * element.count);
-        sum += (price[index] * element.count);
-    });
-    const totalSum = formatPrice(sum);
     res.render('revenue/week', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong tuần', data, totalSum });
 });
 
 router.get('/month', (req, res, next) => {
-    res.render('revenue/month', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong tháng' });
+    res.render('revenue/month', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong tháng', data, totalSum, years});
 });
 
 router.get('/quarter', (req, res, next) => {
-    res.render('revenue/quarter', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong quý' });
+    res.render('revenue/quarter', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong quý', data, totalSum, years });
 });
 
 router.get('/year', (req, res, next) => {
-    res.render('revenue/year', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong năm' });
+    res.render('revenue/year', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong năm', data, totalSum, years});
 });
 
 

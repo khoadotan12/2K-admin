ec83ece3a17952f422f333dd1dbbd4dde1f12cd2
Dong Kha <dongkha97@gmail.com>
2019-04-29 18:42:52 +0700

    add home controllers and order controllers

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const homeControllers = require('../controllers/homeControllers');
+const express = require('express');
+const router = express.Router();
+
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  const data = {
-    revenue: '1,062,231,000',
-    users: 1717,
-    products: 765,
-    orders: 4166,
-  };
-  res.render('dashboard/index', { title: 'Trang chủ', data});
-});
+router.get('/', homeControllers.home);
 
 module.exports = router;

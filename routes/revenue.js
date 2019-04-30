00bc23ec5270931c06f0dc9f296a2c280df951b7
Dong Kha <dongkha97@gmail.com>
2019-04-30 09:25:12 +0700

    update revenue controllers

--- routes/revenue.js
+++ routes/revenue.js
@@ -1,67 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const revenueControllers = require('../controllers/revenueControllers');
 
-function formatPrice(price) {
-    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+router.get('/date', revenueControllers.date);
 
-const sumArr = arr => arr.reduce((a, b) => a + b, 0);
-const price = [6910000, 13940000, 31059000, 1099000, 9401000];
-const data = [
-    {
-        name: 'Samsung Galaxy A7',
-        price: formatPrice(price[0]),
-        count: 18,
-    },
-    {
-        name: 'Xiaomi Mi Pro',
-        price: formatPrice(price[1]),
-        count: 9,
-    },
-    {
-        name: 'iPhone XS Max 256 GB',
-        price: formatPrice(price[2]),
-        count: 22,
-    },
-    {
-        name: 'Oppo Find X',
-        price: formatPrice(price[3]),
-        count: 10,
-    },
-    {
-        name: 'Samsung Galaxy A9',
-        price: formatPrice(price[4]),
-        count: 6,
-    },
-];
-let sum = 0;
-data.forEach((element, index) => {
-    element.id = (index + 1).toString();
-    element.total = formatPrice(price[index] * element.count);
-    sum += (price[index] * element.count);
-});
-const totalSum = formatPrice(sum);
-const years = [2016, 2017, 2018];
+router.get('/week', revenueControllers.week);
 
-router.get('/date', (req, res, next) => {
-    res.render('revenue/date', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong ngày', data, totalSum });
-});
+router.get('/month', revenueControllers.month);
 
-router.get('/week', (req, res, next) => {
-    res.render('revenue/week', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong tuần', data, totalSum });
-});
+router.get('/quarter', revenueControllers.quarter);
 
-router.get('/month', (req, res, next) => {
-    res.render('revenue/month', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong tháng', data, totalSum, years});
-});
-
-router.get('/quarter', (req, res, next) => {
-    res.render('revenue/quarter', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong quý', data, totalSum, years });
-});
-
-router.get('/year', (req, res, next) => {
-    res.render('revenue/year', { category: 'Doanh thu', categoryLink: '/', title: 'Doanh thu trong năm', data, totalSum, years});
-});
+router.get('/year', revenueControllers.year);
 
 
 module.exports = router;

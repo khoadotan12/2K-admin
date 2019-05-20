51c4017e690e679e6f13ba65d8c4afbedafebc46
Dong Kha <dongkha97@gmail.com>
2019-05-20 22:02:31 +0700

    complete create order page

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,8 +1,34 @@
 const orderModel = require('../models/order');
+const productModel = require('../models/product');
+const userModel = require('../models/user');
 
 exports.done = async (req, res, next) => {
     const data = await orderModel.list(2);
     res.render('order/done', { category: 'Đơn hàng', categoryLink: '/order/done', title: 'Đơn hàng đã giao', data })
+};
+
+exports.add = async (req, res, next) => {
+    const data = await productModel.getAll();
+    const users = await userModel.list();
+    res.render('order/add', { category: 'Đơn hàng', categoryLink: '#', title: 'Tạo đơn hàng', data, users });
+}
+
+exports.addPost = async (req, res, next) => {
+    const data = req.body;
+    data.items = [];
+    data.items.push({ productID: data.item, count: 1 });
+    data.create = new Date();
+    data.status = 0;
+    const productInfo = await productModel.getID(data.item);
+    if (productInfo)
+        data.price = productInfo.price;
+    if (data.item)
+        delete data.item;
+    return orderModel.add(data, (error) => {
+        if (error)
+            return res.status(500).send(eror);
+        return res.redirect('/order/receive');
+    });
 };
 
 exports.delivering = async (req, res, next) => {
@@ -30,5 +56,5 @@ exports.changeState = async (req, res, next) => {
 };
 
 exports.info = async (req, res, next) => {
-    
+
 };
--- routes/order.js
+++ routes/order.js
@@ -4,11 +4,13 @@ const orderControllers = require('../controllers/orderControllers');
 
 router.get('/done', orderControllers.done);
 
+router.get('/add', orderControllers.add);
+
+router.post('/', orderControllers.addPost);
+
 router.get('/delivering', orderControllers.delivering);
 
 router.get('/receive', orderControllers.receive);
-
-router.get('/:id', orderControllers.info);
 
 router.get('/cancel', orderControllers.cancel);
 
